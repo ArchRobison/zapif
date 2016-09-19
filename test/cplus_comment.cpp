41c519580683e08f1f39a27c4ef5ef1f29734bcf
Arch D. Robison <archr@nvidia.com>
2016-09-18 20:11:37 -0700

    Fix handling of C++ comments.

--- test/cplus_comment.cpp
+++ test/cplus_comment.cpp
@@ -3,3 +3,17 @@ yes
 #else
 no
 #endif
+
+#if one
+// foo '\\', '\t', '\n', '"', bar
+#endif
+
+#if one
+// really '\\' \
+   long comment
+line above is in comment, but not this line
+#endif
+
+// next line is continuation line \
+#if one
+but not this line
